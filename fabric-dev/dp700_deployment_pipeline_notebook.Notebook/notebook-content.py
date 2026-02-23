6c9e04aeea067ac1f0bfe233edaf7460c640edf1
mouhamadougueyetechniir <mouhamadou.gueye.pro@gmail.com>
2026-02-23 12:55:22 +0100

    Committing 1 item from workspace 0f5b1bcf-30c5-4967-9c17-b8388ad75799

---
+++ fabric-dev/dp700_deployment_pipeline_notebook.Notebook/notebook-content.py
@@ -0,0 +1,28 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# MARKDOWN ********************
+
+# # Deployment pipeline in microsoft fabric
+# * Creattion of a workspace for each environment dev, test, uat and production
+# * Create a git repository and connect workspace with git
+# * Create a deployment pipeline select the corresponding workspace on eaach stage
+
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
